c1fbb7948b092e1c3a5ceac1f28b1fbe27b8d77c
devkai4 <devkai4@pm.me>
2024-11-12 10:53:54 +0900

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from dotenv import load_dotenv
+from send_email import send_email
 import os
 
 load_dotenv()
@@ -14,6 +15,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
