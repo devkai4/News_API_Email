89725eed3a4eb5928341388fa133610932a9041e
devkai4 <devkai4@pm.me>
2024-11-13 10:34:54 +0900

    Add links, specify language, limit news, add subject

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
+import os
 from dotenv import load_dotenv
 from send_email import send_email
-import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
 
 api_key = os.getenv("NEWS_API_KEY")
-url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}&language=en"
 
 # Make request
 request = requests.get(url)
@@ -14,11 +16,17 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+# Set header for email's title
+msg = MIMEMultipart()
+msg['Subject'] = "Today's news"
+
 # Access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body += article["title"] + "\n" \
+              + article["description"] + "\n" \
+              + article["url"] + 2*"\n"
 
-body = body.encode("utf-8")
-send_email(message=body)
+msg.attach(MIMEText(body, 'plain'))
+send_email(message=msg.as_string())
